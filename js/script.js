889693472e6196bf65d313d4bd333048c53512f8
Slav <itrshv@me.com>
2019-10-12 11:14:52 -0400

    Added projects

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
-$(function() {
+$(function () {
     $(window).scroll(function () {
-       if ($(this).scrollTop() > 712) {
-          $('body #navbar ').addClass('colorBack')
-       }
-       if ($(this).scrollTop() < 712) {
-          $('body #navbar').removeClass('colorBack')
-       }
+        if ($(this).scrollTop() > 712) {
+            $('body #navbar ').addClass('colorBack')
+        }
+        if ($(this).scrollTop() < 712) {
+            $('body #navbar').removeClass('colorBack')
+        }
     });
- });
+});
 
- $(document).on('click', 'a[href^="#"]', function (event) {
+$('#skills-button').click(function () {
+    $('#education-description').fadeOut(400)
+    $('#aboutme-description').fadeOut(400)
+    $('#skills-buttona').addClass('active-button')
+    $('#aboutme-buttona').removeClass('active-button')
+    $('#education-buttona').removeClass('active-button')
+
+    setTimeout(function () {
+        $('#skills-description').fadeIn()
+
+    }, 400)
+})
+$('#education-button').click(function () {
+    $('#aboutme-description').fadeOut(400)
+    $('#skills-description').fadeOut(400)
+    $('#education-buttona').addClass('active-button')
+    $('#skills-buttona').removeClass('active-button')
+    $('#aboutme-buttona').removeClass('active-button')
+
+    setTimeout(function () {
+        $('#education-description').fadeIn()
+    }, 400)
+})
+$('#aboutme-button').click(function () {
+    $('#education-description').fadeOut(400)
+    $('#skills-description').fadeOut(400)
+    $('#aboutme-buttona').addClass('active-button')
+    $('#skills-buttona').removeClass('active-button')
+    $('#education-buttona').removeClass('active-button')
+
+    setTimeout(function () {
+        $('#aboutme-description').fadeIn()
+
+    }, 400)
+})
+
+$(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
@@ -21,7 +57,7 @@ var granimInstance = new Granim({
     element: '#canvas-basic',
     direction: 'diagonal',
     isPausedWhenNotInView: true,
-    states : {
+    states: {
         "default-state": {
             gradients: [
                 ['#ff9966', '#ff5e62'],
@@ -35,22 +71,8 @@ var granimInstance = new Granim({
 AOS.init();
 
 var options = {
-    strings: ["","Hi, I'm Slav ^1000 <br> I'm a Computer Science Student"],
+    strings: ["", "Hi, I'm Slav ^1000 <br> I'm a Computer Science Student"],
     typeSpeed: 90
-  }
-  
+}
+
 var typed = new Typed(".element", options);
-
-// var typed = new Typed('#typed', {
-//     stringsElement: '#typed-strings',
-//     typeSpeed: 40
-// });
-
-// var typed2 = new Typed('#typed2', {
-//     stringsElement: '#typed-strings2',
-//     startDelay: 2000,
-//     typeSpeed: 40
-// });
-
-
-
