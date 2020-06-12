e5a2971ed4e14f19d3b65e90dcd43ec1330f7e07
Slav <itrshv@me.com>
2020-06-11 20:14:28 -0400

    Boilerplate for new website

--- js/script.js
+++ js/script.js
@@ -1,64 +1,6 @@
-let navbar_adjust = 890;
+AOS.init();
 
-function myFunction(x) {
-	if (x.matches) {
-		// If media query matches
-		navbar_adjust = 807;
-	}
-}
-
-var x = window.matchMedia('(max-width: 800px)');
-myFunction(x); // Call listener function at run time
-x.addListener(myFunction); // Attach listener function on state changes
-
-$(function() {
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > navbar_adjust) {
-			$('body #navbar ').addClass('colorBack', 1000, 'easeOutBounce');
-		}
-		if ($(this).scrollTop() < navbar_adjust) {
-			$('body #navbar').removeClass('colorBack', 1000, 'easeOutBounce');
-		}
-	});
-});
-
-$('[data-aos]')
-	.parent()
-	.addClass('hideOverflowOnMobile');
-
-$('#skills-button').click(function() {
-	$('#education-description').fadeOut(400);
-	$('#aboutme-description').fadeOut(400);
-	$('#skills-buttona').addClass('active-button');
-	$('#aboutme-buttona').removeClass('active-button');
-	$('#education-buttona').removeClass('active-button');
-
-	setTimeout(function() {
-		$('#skills-description').fadeIn();
-	}, 400);
-});
-$('#education-button').click(function() {
-	$('#aboutme-description').fadeOut(400);
-	$('#skills-description').fadeOut(400);
-	$('#education-buttona').addClass('active-button');
-	$('#skills-buttona').removeClass('active-button');
-	$('#aboutme-buttona').removeClass('active-button');
-
-	setTimeout(function() {
-		$('#education-description').fadeIn();
-	}, 400);
-});
-$('#aboutme-button').click(function() {
-	$('#education-description').fadeOut(400);
-	$('#skills-description').fadeOut(400);
-	$('#aboutme-buttona').addClass('active-button');
-	$('#skills-buttona').removeClass('active-button');
-	$('#education-buttona').removeClass('active-button');
-
-	setTimeout(function() {
-		$('#aboutme-description').fadeIn();
-	}, 400);
-});
+$('[data-aos]').parent().addClass('hideOverflowOnMobile');
 
 $(document).on('click', 'a[href^="#"]', function(event) {
 	event.preventDefault();
@@ -77,19 +19,13 @@ var granimInstance = new Granim({
 	isPausedWhenNotInView: true,
 	states: {
 		'default-state': {
-			gradients: [
-				['#ff9966', '#ff5e62'],
-				['#00F260', '#0575E6'],
-				['#e1eec3', '#f05053']
-			]
+			gradients: [ [ '#ff9966', '#ff5e62' ], [ '#00F260', '#0575E6' ], [ '#e1eec3', '#f05053' ] ]
 		}
 	}
 });
 
-AOS.init();
-
 var options = {
-	strings: ['', "Hi, I'm Slav! ^800 <br> I'm a Computer Science Student."],
+	strings: [ '', "Hi, I'm Slav! ^800 <br> I'm a Computer Science Student." ],
 	typeSpeed: 90
 };
 
@@ -97,23 +33,23 @@ var typed = new Typed('.element', options);
 
 particlesJS('particles-js', {
 	particles: {
-		number: {value: 150, density: {enable: true, value_area: 800}},
-		color: {value: '#ffffff'},
+		number: { value: 150, density: { enable: true, value_area: 800 } },
+		color: { value: '#ffffff' },
 		shape: {
 			type: 'circle',
-			stroke: {width: 0, color: '#000000'},
-			polygon: {nb_sides: 5},
-			image: {src: 'img/github.svg', width: 100, height: 100}
+			stroke: { width: 0, color: '#000000' },
+			polygon: { nb_sides: 5 },
+			image: { src: 'img/github.svg', width: 100, height: 100 }
 		},
 		opacity: {
 			value: 0.8,
 			random: false,
-			anim: {enable: false, speed: 1, opacity_min: 0.1, sync: false}
+			anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
 		},
 		size: {
 			value: 4,
 			random: true,
-			anim: {enable: false, speed: 40, size_min: 0.1, sync: false}
+			anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
 		},
 		line_linked: {
 			enable: false,
@@ -130,18 +66,18 @@ particlesJS('particles-js', {
 			straight: false,
 			out_mode: 'out',
 			bounce: false,
-			attract: {enable: false, rotateX: 600, rotateY: 1200}
+			attract: { enable: false, rotateX: 600, rotateY: 1200 }
 		}
 	},
 	interactivity: {
 		detect_on: 'canvas',
 		events: {
-			onhover: {enable: true, mode: 'bubble'},
-			onclick: {enable: true, mode: 'repulse'},
+			onhover: { enable: true, mode: 'bubble' },
+			onclick: { enable: true, mode: 'repulse' },
 			resize: true
 		},
 		modes: {
-			grab: {distance: 400, line_linked: {opacity: 1}},
+			grab: { distance: 400, line_linked: { opacity: 1 } },
 			bubble: {
 				distance: 100,
 				size: 5,
@@ -149,9 +85,9 @@ particlesJS('particles-js', {
 				opacity: 10,
 				speed: 3
 			},
-			repulse: {distance: 80, duration: 0.4},
-			push: {particles_nb: 4},
-			remove: {particles_nb: 2}
+			repulse: { distance: 80, duration: 0.4 },
+			push: { particles_nb: 4 },
+			remove: { particles_nb: 2 }
 		}
 	},
 	retina_detect: true
@@ -160,7 +96,7 @@ particlesJS('particles-js', {
 const burst = new mojs.Burst({
 	left: 0,
 	top: 0,
-	radius: {4: 60},
+	radius: { 4: 60 },
 	angle: 45,
 	children: {
 		shape: 'line',
@@ -168,12 +104,12 @@ const burst = new mojs.Burst({
 		scale: 2,
 		stroke: '#FD7932',
 		strokeDasharray: '100%',
-		strokeDashoffset: {'-100%': '100%'},
+		strokeDashoffset: { '-100%': '100%' },
 		duration: 700,
 		easing: 'quad.out'
 	}
 });
 
 document.addEventListener('click', function(e) {
-	burst.tune({x: e.pageX, y: e.pageY}).replay();
+	burst.tune({ x: e.pageX, y: e.pageY }).replay();
 });
